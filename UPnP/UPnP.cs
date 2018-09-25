bef270b06feb59b5c019fb8766420da3132a2422
Cristian Ciorba <cristian@firestorm.ro>
2018-09-25 22:00:49 +0300

    - moved uPnP support to IXICore - added uPnP dependencies for S2

---
+++ UPnP/UPnP.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using DLT.Meta;
+using Open.Nat;
+
+namespace DLT
+{
+    class UPnP
+    {
+        private NatDiscoverer natDiscoverer;
+        private NatDevice routerDevice;
+        private IPAddress mappedLocalIP;
+        private int mappedPublicPort;
+
+        public UPnP()
+        {
+            natDiscoverer = new NatDiscoverer();
+        }
+
+        private bool acquireRouterDevice()
+        {
+            if (routerDevice != null)
+            {
+                return true;
+            }
+            try
+            {
+                CancellationTokenSource cts = new CancellationTokenSource();
+                cts.CancelAfter(4500);
+                Task<NatDevice> devicesDiscoveryTask = natDiscoverer.DiscoverDeviceAsync(PortMapper.Upnp, cts);
+                if (devicesDiscoveryTask.Wait(5000) == true)
+                {
+                    NatDevice device = devicesDiscoveryTask.Result;
+                    Logging.info(String.Format("Found UPnP device: {0}", device.ToString()));
+                    routerDevice = device;
+                    return true;
+                }
+            }
+            catch (AggregateException) { }
+            return false;
+        }
+
+        private Mapping GetPublicPortMappingInternal(int public_port)
+        {
+            if (acquireRouterDevice() == true)
+            {
+                try
+                {
+                    Task<IEnumerable<Mapping>> mappings = routerDevice.GetAllMappingsAsync();
+                    if (mappings.Wait(5000) == true)
+                    {
+                        foreach (Mapping m in mappings.Result)
+                        {
+                            if (m.PublicPort == public_port)
+                            {
+                                return m;
+                            }
+                        }
+                    }
+                }
+                catch (MappingException ex)
+                {
+                    Logging.warn(String.Format("Error while obtaining current port mapping: {0}", ex.Message));
+                }
+            }
+            return null;
+        }
+
+        public IPAddress GetExternalIPAddress()
+        {
+            Logging.info("Attempting to discover external address. This is automatic, if the router supports UPnP.");
+            Logging.info("This may take up to 10 seconds...");
+            if (acquireRouterDevice() == true)
+            {
+                Logging.info(String.Format("Found UPnP device: {0}", routerDevice.ToString()));
+                try
+                {
+                    Task<IPAddress> getAddressTask = routerDevice.GetExternalIPAsync();
+                    if (getAddressTask.Wait(5000) == true)
+                    {
+                        IPAddress externalIP = getAddressTask.Result;
+                        Logging.info(String.Format("Found external IP address: {0}", externalIP.ToString()));
+                        return externalIP;
+                    }
+                    else
+                    {
+                        Logging.info(String.Format("UPnP router is not responding: {0}", routerDevice.ToString()));
+                    }
+                }
+                catch (MappingException ex)
+                {
+                    Logging.warn(String.Format("Error while retrieving the external IP: {0}", ex.Message));
+                }
+            }
+            //
+            Logging.info("UPnP router is not present or is using an incompatible version of the UPnP protocol.");
+            return null;
+        }
+
+        public IPAddress GetPublicPortMapping(int public_port)
+        {
+            if (public_port < 0 || public_port > 65535)
+            {
+                Logging.error(String.Format("Invalid port number: {0}", public_port));
+                return null;
+            }
+            Logging.info(String.Format("Attempting to discover existing NAT port mapping for port {0}.", public_port));
+            if (acquireRouterDevice() == true)
+            {
+                Mapping m = GetPublicPortMappingInternal(public_port);
+                return m.PrivateIP;
+            }
+            Logging.info("UPnP router is not present or is using an incompatible version of the UPnP protocol.");
+            return null;
+        }
+
+        public bool MapPublicPort(int public_port, IPAddress local_ip)
+        {
+            if (public_port < 0 || public_port > 65535)
+            {
+                Logging.error(String.Format("Invalid port number: {0}", public_port));
+                return false;
+            }
+            Logging.info(String.Format("Attempting to map external port {0} to local IP {1}", public_port, local_ip.ToString()));
+            if (acquireRouterDevice() == true)
+            {
+                try
+                {
+                    Mapping m = new Mapping(Protocol.Tcp, local_ip, public_port, public_port, 0, "Ixian DLT automatic port mapping");
+                    Task mapPortTask = routerDevice.CreatePortMapAsync(m);
+                    if (mapPortTask.Wait(5000) == true)
+                    {
+                        Logging.info(String.Format("External port successfully {0} mapped to {1}:{2} via UPnP", public_port, local_ip.ToString(), public_port));
+                        mappedLocalIP = local_ip;
+                        mappedPublicPort = public_port;
+                        return true;
+                    }
+                }
+                catch (MappingException ex)
+                {
+                    Logging.error(String.Format("Error while mapping public port {0} to {1}:{2}: {3}", public_port, local_ip.ToString(), public_port, ex.Message));
+                }
+            }
+            Logging.info("UPnP router is not present or is using an incompatible version of the UPnP protocol.");
+            return false;
+        }
+
+        // Returns the local mapped IP
+        public string getMappedIP()
+        {
+            return string.Format("{0}", mappedLocalIP);
+        }
+
+        public void RemoveMapping()
+        {
+            if (routerDevice != null)
+            {
+                if (mappedLocalIP != null)
+                {
+                    Logging.info(String.Format("Removing previously mapped: {0} -> {1}:{2}", mappedPublicPort, mappedLocalIP.ToString(), mappedPublicPort));
+                    try
+                    {
+                        Mapping m = GetPublicPortMappingInternal(mappedPublicPort);
+                        if (m != null)
+                        {
+                            Task deleteMapTask = routerDevice.DeletePortMapAsync(m);
+                            deleteMapTask.Wait(5000);
+                            mappedPublicPort = 0;
+                            mappedLocalIP = null;
+                        }
+                    }
+                    catch (MappingException ex)
+                    {
+                        Logging.error(String.Format("Unable to remove port mapping for public port {0} to {1}:{2}: {3}",
+                            mappedPublicPort,
+                            mappedLocalIP.ToString(), mappedPublicPort,
+                            ex.Message));
+                    }
+                }
+            }
+        }
+    }
+}
