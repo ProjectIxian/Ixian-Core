6b22ce5dcefb37ba43c8a49f84b9e7249dca9ab3
Cristian Ciorba <cristian@firestorm.ro>
2018-09-26 00:21:55 +0300

    - added PresenceStorage to IxianCore - moved NetworkUtils to IxianCore - added maximumNodeReconnectCount support for S2 - added simultaneousConnectedNodes support for S2 - added maximumStreamServerClients support for S2 - updated NetworkClient code for S2 to match DLT NetworkClient features - improved NetworkClientManager for S2

---
+++ Network/NetworkUtils.cs
@@ -0,0 +1,48 @@
+﻿using DLT.Meta;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLT
+{
+    namespace Network
+    {
+        public class NetworkUtils
+        {
+
+            public static string resolveHostname(string hostname)
+            {
+                try
+                {
+                    IPHostEntry hostEntry;
+                    hostEntry = Dns.GetHostEntry(hostname);
+
+                    // TODO: handle multi-ip hostnames
+                    foreach (var ip in hostEntry.AddressList)
+                    {
+                        // TODO: handle IPv6 as well
+                        if (ip.AddressFamily == AddressFamily.InterNetwork)
+                        {
+                            return ip.ToString();
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    return hostname;
+                }
+
+                return "";
+            }
+
+
+        }
+
+    }
+
+}
---
+++ Presence/PresenceStorage.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Linq;
+using DLT.Meta;
+
+namespace DLT
+{
+    class PresenceStorage
+    {
+        public static string presenceFilename = "presence.dat";
+
+        // Saves a list of 500 master node addresses to a file
+        public static void savePresenceFile()
+        {
+            // Get a list of master node presences
+            List<string> mn_presences = PresenceList.getMasterNodeAddresses(500);
+            // Don't write to file if no masternode presences were found in addition to the current node
+            if (mn_presences.Count < 2)
+                return;
+
+            using (TextWriter tw = new StreamWriter(presenceFilename))
+            {
+                foreach (String addr in mn_presences)
+                    tw.WriteLine(addr);
+            }
+            mn_presences.Clear();
+        }
+
+        // Retrieves the master node address file's contents
+        public static List<string> readPresenceFile()
+        {
+            List<string> mn_presences = new List<string>();
+
+            // Check if the presence file exists
+            if (File.Exists(presenceFilename))
+            {
+                Logging.info("Presence file found. Adding addresses to initial connections.");
+            }
+            else
+            {
+                return mn_presences;
+            }
+
+            try
+            {
+                mn_presences = File.ReadAllLines(presenceFilename).ToList();
+            }
+            catch (Exception)
+            {
+
+            }
+
+            return mn_presences;
+        }
+
+        // Deletes the presence file cache
+        public static void deletePresenceFile()
+        {
+            if (File.Exists(PresenceStorage.presenceFilename))
+            {
+                File.Delete(PresenceStorage.presenceFilename);
+            }
+
+            return;
+        }
+
+    }
+}
