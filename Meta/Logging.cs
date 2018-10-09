de4a5fe93152a3370cdbea39742c33d16ccdf375
Cristian Ciorba <cristian@firestorm.ro>
2018-10-09 12:24:38 +0300

    - added rolling log files - removed TextWriter implementation in logging to allow moving log files during runtime

--- Meta/Logging.cs
+++ Meta/Logging.cs
@@ -2,6 +2,9 @@ using System;
 using System.IO;
 using System.Diagnostics;
 using System.Threading;
+using System.Reflection;
+using System.Linq;
+using System.Text;
 
 namespace DLT
 {
@@ -17,18 +20,34 @@ namespace DLT
         public class Logging
         {
             private static Logging singletonInstance;
-            private TextWriter outputFile = null;
             private LogSeverity currentSeverity;
-            private static string logfilename = "ixian.log";
 
+
+            private static string logfilename = "ixian.log";
+            private static string logfilepath = ""; // Stores the full log path
+            private static string folderpath = ""; // Stores just the folder path
+            private static string wildcard = "*";
+            private static string logfilepathpart = "";
 
             private Logging()
             {
                 currentSeverity = LogSeverity.trace;
                 try
                 {
-                    outputFile = File.AppendText(logfilename);
-                } catch(Exception e)
+                    // Obtain paths and cache them
+                    folderpath = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+                    logfilepath = Path.Combine(folderpath, Path.GetFileNameWithoutExtension(logfilename));
+                    wildcard = Path.GetFileNameWithoutExtension(logfilename) + "*" + Path.GetExtension(logfilename);
+                    logfilepathpart = Path.Combine(folderpath, Path.GetFileNameWithoutExtension(logfilename));
+
+                    // Clear all previous logs
+                    clear();
+
+                    // Create the main log file
+                    File.AppendAllText(logfilename, "Ixian Log" + Environment.NewLine, Encoding.UTF8);
+
+                }
+                catch (Exception e)
                 {
                     // Ignore all exception and start anyway with console only logging.
                     Console.WriteLine(String.Format("Unable to open log file. Error was: {0}. Logging to console only.", e.Message));
@@ -67,24 +86,76 @@ namespace DLT
 
                     Debug.WriteLine(formattedMessage);
 
-                    if (Logging.singleton.outputFile != null)
+                    lock (logfilename)
                     {
-                        Logging.singleton.outputFile.WriteLine(formattedMessage);
-                        Logging.singleton.outputFile.Flush();
+                        Logging.roll();
+                        File.AppendAllText(logfilename, formattedMessage + Environment.NewLine, Encoding.UTF8);
                     }
 
                 }
             }
 
-            // Clears the log file
+            // Rolls the log file
+            public static void roll()
+            {
+                try
+                {
+                    var length = new FileInfo(logfilename).Length;
+                    if(length > Config.maxLogFileSize)
+                    {
+                        Console.WriteLine("Path: {0}", folderpath);
+
+                        string[] logFileList = Directory.GetFiles(folderpath, wildcard, SearchOption.TopDirectoryOnly);
+                        if (logFileList.Length > 0)
+                        {
+                            // + 2 because of the . and digit [0-9]
+                            var rolledLogFileList = logFileList.Where(fileName => Path.GetFileName(fileName).Length == (logfilename.Length + 2)).ToArray();
+                            Array.Sort(rolledLogFileList, 0, rolledLogFileList.Length);
+                            if (rolledLogFileList.Length >= 10)
+                            {
+                                File.Delete(rolledLogFileList[9]);
+                                var list = rolledLogFileList.ToList();
+                                list.RemoveAt(9);
+                                rolledLogFileList = list.ToArray();
+                            }
+                            // move remaining rolled files
+                            for (int i = rolledLogFileList.Length; i > 0; --i)
+                                File.Move(rolledLogFileList[i - 1], logfilepathpart + "." + i + Path.GetExtension(logfilename));
+                            var targetPath = logfilepathpart + ".0" + Path.GetExtension(logfilename);
+                            // move original file
+                            File.Move(logfilename, targetPath);
+                        }
+                    }
+
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine("Exception rolling log file: {0}", e.Message);
+                }
+            }
+
+            // Clears the log files
             public static void clear()
             {
-                Logging.singleton.outputFile.Close();
-                if (File.Exists(logfilename))
+                lock (logfilename)
                 {
-                    File.Delete(logfilename);
+
+                    if (File.Exists(logfilename))
+                    {
+                        File.Delete(logfilename);
+                    }
+
+                    string[] logFileList = Directory.GetFiles(folderpath, wildcard, SearchOption.TopDirectoryOnly);
+                    var rolledLogFileList = logFileList.Where(fileName => Path.GetFileName(fileName).Length == (logfilename.Length + 2)).ToArray();
+
+                    for (int i = rolledLogFileList.Length; i >= 0; --i)
+                    {
+                        string filename = logfilepathpart + "." + i + Path.GetExtension(logfilename);
+                        if (File.Exists(filename))
+                            File.Delete(filename);
+                    }
+
                 }
-                Logging.singleton.outputFile = File.AppendText(logfilename);
             }
 
             #region Convenience methods
