f18309293a456881b43be86c34839cd76636d9da
Cristian Ciorba <cristian@firestorm.ro>
2018-10-18 02:13:39 +0300

    - changed legacy block threshold

--- Legacy/Legacy.cs
+++ Legacy/Legacy.cs
@@ -9,7 +9,7 @@ namespace DLT
     class Legacy
     {
         // List of upgrades and corresponding block heights
-        public static readonly ulong up20181017 = 20; 
+        public static readonly ulong up20181017 = 15; 
 
 
 
