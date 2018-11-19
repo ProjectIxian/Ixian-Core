6822e56a29b0cc369b313089ad11879842f728a4
Cristian Ciorba <cristian@firestorm.ro>
2018-11-19 23:56:30 +0200

    - added convenience function for CryptoManager init

--- Crypto/CryptoManager.cs
+++ Crypto/CryptoManager.cs
@@ -24,5 +24,11 @@ namespace DLT
             lib = new CryptoLib(new CryptoLibs.BouncyCastle());
         }
 
+        // Initialize with a specific crypto library
+        public static void initLib(ICryptoLib crypto_lib)
+        {
+            lib = new CryptoLib(crypto_lib);
+        }
+
     }
 }
