29df6705c61e149e0befb2244a7cd5e1425195db
MarkoAT <no-reply@ixian.io>
2019-09-12 01:12:24 +0200

    - temporarily moved SpixiMessage and StreamMessage to IxiCore/Streaming

--- Meta/CoreConfig.cs
+++ Meta/CoreConfig.cs
@@ -8,7 +8,7 @@
         /// <summary>
         /// Current version of IxiCore.
         /// </summary>
-        public static readonly string version = "xcore-0.6.6e";
+        public static readonly string version = "xcore-0.6.6f-dev";
 
         /// <summary>
         /// Current version of the Ixian network protocol.
