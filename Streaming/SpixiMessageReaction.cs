bdd450ea19d16fb1c644342699ec13f02bbbd57c
MarkoAT <no-reply@ixian.io>
2020-07-19 04:44:45 +0200

    - added SpixiMessageReaction class for reactions support

---
+++ Streaming/SpixiMessageReaction.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+
+namespace IXICore
+{
+    public class SpixiMessageReaction
+    {
+        public byte[] msgId = null;
+        public string reaction = null;
+
+        public SpixiMessageReaction(byte[] msg_id, string reaction)
+        {
+            msgId = msg_id;
+            this.reaction = reaction;
+        }
+
+        public SpixiMessageReaction(byte[] reaction_bytes)
+        {
+            using (MemoryStream m = new MemoryStream(reaction_bytes))
+            {
+                using (BinaryReader reader = new BinaryReader(m))
+                {
+                    int msg_id_len = reader.ReadInt32();
+                    msgId = reader.ReadBytes(msg_id_len);
+                    reaction = reader.ReadString();
+                }
+            }
+        }
+
+        public byte[] getBytes()
+        {
+            using (MemoryStream m = new MemoryStream())
+            {
+                using (BinaryWriter writer = new BinaryWriter(m))
+                {
+                    writer.Write(msgId.Length);
+                    writer.Write(msgId);
+                    writer.Write(reaction);
+                }
+                return m.ToArray();
+            }
+        }
+    }
+}
