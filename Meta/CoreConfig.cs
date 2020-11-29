c785635d10f2bfa0e2cc57eb0071ae78543eb09d
MarkoAT <no-reply@ixian.io>
2020-11-29 04:55:48 +0100

    xcore-0.7.5f

--- Meta/CoreConfig.cs
+++ Meta/CoreConfig.cs
@@ -8,7 +8,7 @@ namespace IXICore
         /// <summary>
         /// Current version of IxiCore.
         /// </summary>
-        public static readonly string version = "xcore-0.7.5d";
+        public static readonly string version = "xcore-0.7.5f";
 
         /// <summary>
         /// Current version of the Ixian network protocol.
