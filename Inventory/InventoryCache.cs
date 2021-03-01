e936f210bb7f0ca561773c17dd3c3c3307f6165b
MarkoAT <no-reply@ixian.io>
2021-03-01 04:18:36 +0100

    changed inventory timeouts

--- Inventory/InventoryCache.cs
+++ Inventory/InventoryCache.cs
@@ -60,8 +60,8 @@ namespace IXICore.Inventory
             inventory.Add(InventoryItemTypes.transaction, new Dictionary<byte[], PendingInventoryItem>(new ByteArrayComparer()));
 
             typeOptions = new Dictionary<InventoryItemTypes, InventoryTypeOptions>();
-            typeOptions.Add(InventoryItemTypes.block, new InventoryTypeOptions() { maxRetries = 5, timeout = 15, maxItems = 100 });
-            typeOptions.Add(InventoryItemTypes.blockSignature, new InventoryTypeOptions() { maxRetries = 5, timeout = 15, maxItems = 2000 });
+            typeOptions.Add(InventoryItemTypes.block, new InventoryTypeOptions() { maxRetries = 5, timeout = 5, maxItems = 100 });
+            typeOptions.Add(InventoryItemTypes.blockSignature, new InventoryTypeOptions() { maxRetries = 5, timeout = 10, maxItems = 2000 });
             typeOptions.Add(InventoryItemTypes.keepAlive, new InventoryTypeOptions() { maxRetries = 2, timeout = 30, maxItems = 10000 });
             typeOptions.Add(InventoryItemTypes.transaction, new InventoryTypeOptions() { maxRetries = 5, timeout = 200, maxItems = 10000 });
         }
