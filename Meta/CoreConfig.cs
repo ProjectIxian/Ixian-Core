2924846cd853462280d8c9f86e426b8d9b0bb0b5
MarkoAT <no-reply@ixian.io>
2022-06-04 14:32:12 +0200

    xcore-0.9.0a-beacon

--- Meta/CoreConfig.cs
+++ Meta/CoreConfig.cs
@@ -20,7 +20,7 @@ namespace IXICore
         /// <summary>
         /// Current version of IxiCore.
         /// </summary>
-        public static readonly string version = "xcore-0.9.0a-beacon-rc1";
+        public static readonly string version = "xcore-0.9.0a-beacon";
 
         /// <summary>
         /// Current version of the Ixian network protocol.
@@ -32,13 +32,13 @@ namespace IXICore
         /// Useful for optimized block header sync
         /// Note: Always round last block height to 1000 and subtract 1 (i.e. if last block height is 33234, the correct value is 32999)
         /// </summary>
-        public static ulong bakedBlockHeight = 2131999;
+        public static ulong bakedBlockHeight = 2272999;
 
         /// <summary>
         /// Mainnet block checksum (paired with bakedBlockHeight) of bakedBlockHeight
         /// Useful for optimized block header sync
         /// </summary>
-        public static byte[] bakedBlockChecksum = Crypto.stringToHash("85deeec4480d0fb4f23dcbc78862d69b07f5045350fee7ea7dee3941b4799f30f180893c0a1c74823a13918e");
+        public static byte[] bakedBlockChecksum = Crypto.stringToHash("3dbb76f7e0edb189b29df414842c90f988791cd55b69339f3414b9b71567486e529f5052c3676ff5dd7cf4a4");
 
         /// <summary>
         /// Number of wallets to send in each chunk of data when synchronizing new Master Nodes.
