9fe6bbdd49d5f7cb6262656b38964b82d869e13d
MarkoAT <no-reply@ixian.io>
2022-06-17 19:00:15 +0200

    xcore-0.9.0a2-beacon

--- Meta/CoreConfig.cs
+++ Meta/CoreConfig.cs
@@ -20,7 +20,7 @@ namespace IXICore
         /// <summary>
         /// Current version of IxiCore.
         /// </summary>
-        public static readonly string version = "xcore-0.9.0a1-beacon";
+        public static readonly string version = "xcore-0.9.0a2-beacon";
 
         /// <summary>
         /// Current version of the Ixian network protocol.
