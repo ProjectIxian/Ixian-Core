1a57c2adf2ec78b910bbce2c8d9424950dbbd5c2
Marko <marko.arh@ixian.io>
2024-12-09 17:27:00 +0100

    xcore-0.9.3a temporary changes to PL PoW calculation parameters

--- Meta/CoreConfig.cs
+++ Meta/CoreConfig.cs
@@ -20,7 +20,7 @@ namespace IXICore
         /// <summary>
         /// Current version of IxiCore.
         /// </summary>
-        public static readonly string version = "xcore-0.9.3";
+        public static readonly string version = "xcore-0.9.3a";
 
         /// <summary>
         /// Current version of the Ixian network protocol.
