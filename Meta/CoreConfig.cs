fc049bcb44ca60d4109958b9d801bfcd6a1acd96
Marko <marko.arh@ixian.io>
2025-01-29 20:40:17 +0100

    xcore-0.9.3e

--- Meta/CoreConfig.cs
+++ Meta/CoreConfig.cs
@@ -20,7 +20,7 @@ namespace IXICore
         /// <summary>
         /// Current version of IxiCore.
         /// </summary>
-        public static readonly string version = "xcore-0.9.3d";
+        public static readonly string version = "xcore-0.9.3e";
 
         /// <summary>
         /// Current version of the Ixian network protocol.
